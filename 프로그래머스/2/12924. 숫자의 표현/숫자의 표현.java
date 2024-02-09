05f8ac50d11f04f5818ba7a49bc8473d98674d85
ShimWooJun <118426613+shimWooJun@users.noreply.github.com>
2024-02-10 02:16:15 +0900

    [level 2] Title: 숫자의 표현, Time: 1.55 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int start = 1;
+        
+        while(start <= n) {
+            int sum = 0;
+            for(int i = start; i <= n; i++){
+                sum += i;
+                if(sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n){
+                    break;
+                }
+            }
+            start++;
+        }
+        return answer;
+    }
+}
